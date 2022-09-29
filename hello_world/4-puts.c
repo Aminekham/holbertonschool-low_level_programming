166e05bad3a8eef775f020fcb8fae910c215e6e1
Aminekham <lkham0508@gmail.com>
2022-09-29 15:30:11 -0700

    lets try first c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    puts("Programming is like building a multilingual puzzle);
-    getchar();
+    puts("Programming is like building a multilingual puzzle"\n);
     return 0;
 }
 
