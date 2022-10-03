a05727886f6288c7066601f3df6e6ba4ddf20db5
Aminekham <lkham0508@gmail.com>
2022-10-03 10:14:14 -0700

    putchar with integers

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point for c program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; ++i)
+{
+		putchar(i);
+}
+	putchar('\n');
+	return (0);
+}
