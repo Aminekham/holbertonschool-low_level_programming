8d53d721168f1587132748ca76730e3644b0460f
Aminekham <lkham0508@gmail.com>
2022-10-03 10:19:03 -0700

    reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point for c program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (i = 122; i < 96; --i)
+{
+		putchar(i);
+}
+	putchar('\n');
+	return (0);
+}
