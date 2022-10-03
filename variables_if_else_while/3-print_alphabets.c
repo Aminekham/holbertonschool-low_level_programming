8e35ec0469d7f141ff62c9163a167a89750b498e
Aminekham <lkham0508@gmail.com>
2022-10-03 07:20:01 -0700

    all

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point for c program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; ++i)
+{
+		putchar(i);
+}
+	putchar('\n');
+	for (i = 65; i < 90; ++i)
+{
+		putchar(i);
+}
+	return (0);
+}
