dd3dd19c16cbbd0bda2826605b85e56554deb6f2
Aminekham <lkham0508@gmail.com>
2022-10-03 05:46:16 -0700

    alph

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio>
+/**
+ * main - Entry point for c program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+	int i;
+
+	for (i = 97; i < 122;++i)
+{
+		putchar(str[i]);
+}
+	return(0);
+}
