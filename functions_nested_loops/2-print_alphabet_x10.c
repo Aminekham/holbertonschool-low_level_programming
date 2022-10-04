0b497918f8c790f15fb2dee4758dafe1308c48a4
Aminekham <lkham0508@gmail.com>
2022-10-04 12:25:47 -0700

    alphabet *10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+ * 2-print_alphabet_x10.c - read the program bellow 
+ *
+ * Return : void
+ */
+void print_alphabet_x10 (void)
+{
+	int i , j;
+
+	for ( i = 0; i <= 10 ; i++)
+{
+		for ( j = 97; j < 123; j++)
+{
+			_putchar(j);
+		_putchar('\n');
+}
+}
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,4 +1,4 @@
 #include <stdio.h>
 int _putchar(int i);
 void print_alphabet(void);
-
+void print_alphabet_x10(void);
