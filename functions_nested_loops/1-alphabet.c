b2cb8106ed9ea0a87fb5bf2731f3caa6e0474a31
Aminekham <lkham0508@gmail.com>
2022-10-04 12:05:08 -0700

    finally

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - check the code
  *
  * Return: Always 0.
  */
@@ -9,7 +9,7 @@ void print_alphabet(void)
 {
 	int i;
 
-	for (i=97 ; i < 123; i++)
+	for (i = 97 ; i < 123; i++)
 {
 		_putchar(i);
 }
