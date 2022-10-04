dd2983831b317a4523f4b51fa9a2b363a0c7c893
Aminekham <lkham0508@gmail.com>
2022-10-04 01:52:18 -0700

    combine

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,13 +8,16 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i < 57; ++i)
+	for (i = 48; i < 58; ++i)
 {
 		putchar(i);
+	if (i != 57)
+{
 		putchar(',');
 		putchar(32);
 }
-	putchar(57);
+}
+
 	putchar('\n');
 	return (0);
 }
