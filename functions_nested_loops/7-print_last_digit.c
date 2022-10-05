84d2bd5330eb4aa6a95bca6e190e71806f4784f1
Aminekham <lkham0508@gmail.com>
2022-10-05 17:55:43 +0200

    last digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,8 +7,10 @@ int print_last_digit(int n)
         r = (n % 10);
         if ( r < 0)
 		{
-        _putchar(r + '0');
+        _putchar(r);
+		_putchar(r);
 		}
+		_putchar(r);
         return (r);
 
 }
