83453f23c9a5bf168079e0f8aec5c0b8bfb30642
Aminekham <lkham0508@gmail.com>
2022-10-05 01:58:35 -0700

    awah

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/*
+ * 2-print_alphabet_x10.c - read the program bellow 
+ *
+ * @c - integer to use
+ *
+ * Return : void
+ */
+int _islower(int c)
+{
+	char f = c;
+
+	if ( islower(f) == 1)
+{
+		return (1);
+}
+	else
+{
+		return (0);
+}
+}
