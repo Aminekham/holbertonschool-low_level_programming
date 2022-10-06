b4c1db1f2305d6d2c7760d919cdde2f087e4a29e
Aminekham <lkham0508@gmail.com>
2022-10-06 12:04:08 +0200

    hours and minutes

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -2,18 +2,18 @@
 
 void jack_bauer(void)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 00; i <= 23; i++ )
-    {
-        for (j = 00; j < 60; j++)
-        {
-            _putchar((i/10) + '0');
-            _putchar((i%10) + '0');
-            _putchar(':');
-            _putchar((j/10) + '0');
-            _putchar((j%10) + '0');
-            _putchar('\n');
-        }
-    }
+	for (i = 00; i <= 23; i++)
+	{
+		for (j = 00; j < 60; j++)
+		{
+			_putchar((i/10) + '0');
+			_putchar((i%10) + '0');
+			_putchar(':');
+			_putchar((j/10) + '0');
+			_putchar((j%10) + '0');
+			_putchar('\n');
+		}
+	}
 }
