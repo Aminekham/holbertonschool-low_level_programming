a58ab5cf1ecd475f2c6a142e3a207654e02da57d
Aminekham <lkham0508@gmail.com>
2022-10-06 10:11:38 +0200

    last with negative

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,14 +1,20 @@
 #include "main.h"
-
+/**
+ *print_last_digit - Description: 'give the last digit'
+ *
+ * @n:  - integer to use
+ *
+ * Return: the last digit
+ */
 int print_last_digit(int n)
 {
-        int r = n % 10;
-;
-        if (r < 0)
+		int r = n % 10;
+        
+		if (r < 0)
 		{
 			r *= -1;
 		}
 		_putchar(r + '0');
-        return (r);
-
+		
+		return (r);
 }
