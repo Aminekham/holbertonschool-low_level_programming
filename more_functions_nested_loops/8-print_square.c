2b4e78157005d1db3261171c516b6c6a156c71c6
Aminekham <lkham0508@gmail.com>
2022-10-07 17:55:59 +0200

    square

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,21 +1,25 @@
 #include "main.h"
-
+/**
+ * print_square - printing all numbers till 14
+ *
+ * @size: - integer to receive the number of lines
+ */
 void print_square(int size)
 {
-    int i, j;
-    if (size > 0)
-    {   
-        for (i = 1; i <= size; i++)
-        {
-            for (j = 1; j <= size; j++)
-            {
-                _putchar('#');
-            }
-            _putchar('\n');
-        }    
-    }
-    else
-    {
-        _putchar('\n');
-    }
+	int i, j;
+	if (size > 0)
+	{   
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}    
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
