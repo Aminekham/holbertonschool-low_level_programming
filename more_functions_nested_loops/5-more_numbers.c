fb21564a86dd412ba142e880318907056646983d
Aminekham <lkham0508@gmail.com>
2022-10-07 14:16:38 +0200

    max : 14

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,14 +1,17 @@
  #include "main.h"
-
+/**
+ * more_numbers - printing all numbers till 14
+ *
+ */
 void more_numbers(void)
 {
-    int i, j;
+	int i, j;
 	for (j = 0; j < 10; j++)
 	{
-        for (i = 0; i < 15; i++)
+		for (i = 0; i < 15; i++)
 		{
-			    _putchar(i/10 + '0');
-            	_putchar(i%10 + '0');
+				_putchar(i/10 + '0');
+				_putchar(i%10 + '0');
 				
 		}
 		_putchar('\n');
