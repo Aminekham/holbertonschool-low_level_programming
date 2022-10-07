6a2fd5837756bd933c092306e9f52b60024597c5
Aminekham <lkham0508@gmail.com>
2022-10-07 18:44:05 +0200

    try this solution

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,15 +11,15 @@ int main(void)
 	{
 		if (i % 3 == 0 )
 		{
-			printf("fizz ");
+			printf("Fizz ");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("buzz ");
+			printf("Buzz ");
 		}
 		else if (i % 5 == 0 && i % 3 == 0)
 		{
-			printf("fizzbuzz ");
+			printf("FizzBuzz ");
 		}
 		else 
 		{
