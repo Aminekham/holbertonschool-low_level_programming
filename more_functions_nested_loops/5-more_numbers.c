a09650c494356907a12029d883830709764eaec0
Aminekham <lkham0508@gmail.com>
2022-10-07 14:15:05 +0200

    till 14

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+ #include "main.h"
+
+void more_numbers(void)
+{
+    int i, j;
+	for (j = 0; j < 10; j++)
+	{
+        for (i = 0; i < 15; i++)
+		{
+			    _putchar(i/10 + '0');
+            	_putchar(i%10 + '0');
+				
+		}
+		_putchar('\n');
+	}
+}
