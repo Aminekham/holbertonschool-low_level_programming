087b71efe776b4cb8161a957c695c44f930fbce3
Aminekham <lkham0508@gmail.com>
2022-10-07 14:23:27 +0200

    lets try printing lines

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * more_numbers - printing all numbers till 14
+ *
+ */
+ void print_line(int n) 
+{
+	int i;
+	for (i = 0; i <= n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
