fe5430b991edf872ebad9434bf4dbd06c4fb6121
Aminekham <lkham0508@gmail.com>
2022-10-07 18:30:07 +0200

    square

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * fizzbuzz - inting all numbers till 14
+ *
+ */
+int fizzbuzz(void)
+{
+	for (i = 1; i < 100)
+	{
+		int i;
+
+		if (i % 3 = 0 )
+		{
+			printf("fizz");
+		}
+		else if (i % 5 = 0)
+		{
+			printf("buzz");
+		}
+		else if (i % 5 && i % 3)
+		{
+			printf("fizzbuzz");
+		}
+		else 
+		{
+			printf("%d",i);
+		}
+	}
+}
