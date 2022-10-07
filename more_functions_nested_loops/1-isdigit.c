915f6554e37be72f5d39766689f296a61b15b1ae
Aminekham <lkham0508@gmail.com>
2022-10-07 11:54:41 +0200

    checking on upper and lower

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isdigit - adding two numbers
+ *
+ * @c: - integer to use
+ *
+ * Return: 0 or 1 
+ */
+int _isdigit(int c)
+{
+    if(c => 48 && c <=57 )
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,3 +1,4 @@
 #include <stdio.h>
 int _putchar(int i);
 int _isupper(int c);
+int _isdigit(int c);
