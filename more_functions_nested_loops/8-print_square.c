16b1729d37257542d7bb28173cf7b89d2407805a
Aminekham <lkham0508@gmail.com>
2022-10-07 18:00:19 +0200

    square

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -7,6 +7,7 @@
 void print_square(int size)
 {
 	int i, j;
+
 	if (size > 0)
 	{   
 		for (i = 1; i <= size; i++)
@@ -15,7 +16,7 @@ void print_square(int size)
 			{
 				_putchar('#');
 			}
-			_putchar('\n');
+				_putchar('\n');
 		}    
 	}
 	else
