c65992d6b5fae129724db5679745ffbc9a3f3c3d
Aminekham <lkham0508@gmail.com>
2022-10-07 11:33:00 +0200

    checking on upper and lower

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper(int c) - Description: 'check if the character is uppercase'
+ *
+ * @c: - integer to use
+ *
+ * Return : 0 or 1 (depends on the program)
+ */
+int _isupper(int c) 
+{
+    if (c > 64 && c < 91)
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
+}
---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,3 @@
+#include <stdio.h>
+int _putchar(int i);
+int _isupper(int c);
