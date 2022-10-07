0c5878adc05056a7d9ffba0e4b529bdff2e2154a
Aminekham <lkham0508@gmail.com>
2022-10-07 16:16:11 +0200

    triangle

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,24 +1,24 @@
 #include "main.h"
 /**
- * print_line - printing all numbers till 14
+ * print_diagonal - printing all numbers till 14
  *
  * @n: - integer to receive the number of lines
  */
- void print_diagonal(int n)
+void print_diagonal(int n)
 {
 	int i, j;
-	if (n > 0)
+
+	if(n > 0)
 	{
 		for (i = 0; i < n; i++)
-        {
+		{
 			for (j = 0; j < i; j++)
 			{
 				_putchar(' ');	
 			}
 			_putchar(92);
 			_putchar('\n');	
-		}
-		
+		}	
 	}
 	else
 	{
