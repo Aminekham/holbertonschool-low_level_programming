31b2e03d53794fb3254d030bfefe8406923eaf4b
Aminekham <lkham0508@gmail.com>
2022-10-09 18:07:27 +0200

    triangles final version

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_triangle - Do you want to print an inversed triangle
+ * 
+ * @size: - integer to receive the number of lines
+*/
+void print_triangle(int size)
+{
+	int i, j, n;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j= 1; j < size-i+1; j++)
+		{
+			_putchar(' ');
+		}
+		for (n= size-i; n < size; n++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
