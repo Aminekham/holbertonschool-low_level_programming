ce2da31a178cfc55e24863463a8fa43b54773215
Aminekham <lkham0508@gmail.com>
2022-10-09 14:35:23 +0200

    the more numbers task is actually easy

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -13,9 +13,9 @@ void more_numbers(void)
 		{
 			if (i >= 10)
 			{
-				_putchar((i/10)+ '0');
+				_putchar((i / 10) + '0');
 			}
-			_putchar((i%10) + '0');
+			_putchar((i % 10) + '0');
 		}
 		_putchar('\n');
 	}
