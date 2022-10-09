01d85917c3a97754aa0659c5d995e120f8a668c0
Aminekham <lkham0508@gmail.com>
2022-10-09 14:33:07 +0200

    the more numbers task is actually easy

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -6,13 +6,16 @@
 void more_numbers(void)
 {
 	int i, j;
+
 	for (j = 0; j < 10; j++)
 	{
-		for (i = 0; i < 15; i++)
+		for (i = 0; i <= 14; ++i)
 		{
-				_putchar(i/10 + '0');
-				_putchar(i%10 + '0');
-				
+			if (i >= 10)
+			{
+				_putchar((i/10)+ '0');
+			}
+			_putchar((i%10) + '0');
 		}
 		_putchar('\n');
 	}
