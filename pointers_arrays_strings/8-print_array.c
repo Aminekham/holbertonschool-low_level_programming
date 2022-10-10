ea050267b441f002e33eecaeb438fe5e79897d5f
Aminekham <lkham0508@gmail.com>
2022-10-10 23:04:56 +0200

    pointer to array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * 
+ * 
+ */
+void print_array(int *a, int n)
+{
+	int i;
+	for (i = 0; i <= n; i++)
+	{
+		if (i = n)
+		{
+			printf("%d", *a);
+		}
+	    printf("%d ,", *a);
+		a++;
+	}
+}
