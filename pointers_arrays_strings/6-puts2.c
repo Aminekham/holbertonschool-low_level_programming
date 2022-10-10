790b11d2bbff36698c1f2955e1787feade1a8170
Aminekham <lkham0508@gmail.com>
2022-10-10 19:57:06 +0200

    lets jump

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -9,9 +9,9 @@ void puts2(char *str)
 	int l, i;
 
 	l = strlen(str);
-	for (i = 0; i < l; i+=2)
+	for (i = 0; i < l; i += 2)
 	{
-		_putchar (str [i]);
+		_putchar(str [i]);
 	}
 	_putchar('\n');
 }
