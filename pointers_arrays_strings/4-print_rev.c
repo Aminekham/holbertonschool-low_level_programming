274ed60c107d931e2f927260bf7f7274c4dfb11e
Aminekham <lkham0508@gmail.com>
2022-10-10 16:41:32 +0200

    reverse function

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,17 +1,18 @@
 #include "main.h"
 #include <string.h>
 /**
- * 
- * 
+ * print_rev - this function was made to reverse a string
+ *
+ * @s: - a string to reverse
  */
 void print_rev(char *s)
 {
-    int L, i;
+	int L, i;
 
-    L = strlen(s);
-    for (i = L ; i >= 1; i--)
-    {
-        _putchar(s[i]);
-    }
-    _putchar('\n');
+	L = strlen(s);
+	for (i = L; i >= 1; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
 }
