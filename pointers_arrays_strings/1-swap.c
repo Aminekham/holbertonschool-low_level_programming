483794ed3eba5d5da0ffbf8088d5d337099c5b7a
Aminekham <lkham0508@gmail.com>
2022-10-10 11:43:50 +0200

    lets swap those two numbers

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,13 +1,16 @@
 #include "main.h"
 /**
+ * swap_int - swapping between two pointers
+ *
+ * @a - an integer pointer 
  * 
- * 
-*/
+ * @b - an integer pointer 
+ */
 void swap_int(int *a, int *b)
 {
-    int c;
+	int c;
 
-    c = *a;
-    *a = *b;
-    *b = c;
+	c = *a;
+	*a = *b;
+	*b = c;
 }
