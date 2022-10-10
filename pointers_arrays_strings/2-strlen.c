ee6af3d3781a3a615778521a48a851c263ad6e97
Aminekham <lkham0508@gmail.com>
2022-10-10 12:33:36 +0200

    the length

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * strlen - length of a string
+ *
+ * @s: - the string itself
+ */
+int _strlen(char *s)
+{
+	int i, c;
+	c = ' ';
+	i = 0;
+	while (c != '\0')
+	{
+		c = s [i];
+		i = i + 1;
+	}
+	return (i-1);
+}
