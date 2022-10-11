8b352c0df5e88fe29158d7e1c31b5a4986243644
Aminekham <lkham0508@gmail.com>
2022-10-11 14:07:59 +0200

    pointer inside of an array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,11 +1,15 @@
 #include "main.h"
 /**
+ * print_array - this function is made to print an array
+ *
+ * @a: - variable for the array itself
  * 
- * 
+ * @n: - the length of the array
  */
 void print_array(int *a, int n)
 {
 	int i;
+
 	for (i = 0; i <= n - 1; i++)
 	{
 		if (i == n - 1)
