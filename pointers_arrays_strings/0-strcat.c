685cd7e572bac5909e238faa3acc72b84cccab2f
Aminekham <lkham0508@gmail.com>
2022-10-12 11:38:03 +0200

    concat those two

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,18 +1,21 @@
 #include "main.h"
 #include <string.h>
 /**
- * 
- * 
+ * _strcat - concat those two strings
+ *
+ * @dest: - string to save the whole process
+ *
+ * @src: - the first string
  */
 char *_strcat(char *dest, char *src)
 {
-    int  len, i, mn;
+	int len, i, mn;
 
-    len = strlen (src);
-    mn = strlen (dest);
-    for (i = 0; i <= len; i++ )
-    {
-        dest[mn + i] = src [i];
-    }
-    return (dest);
+	len = strlen(src);
+	mn = strlen(dest);
+	for (i = 0; i <= len; i++)
+	{
+		dest[mn + i] = src[i];
+	}
+	return (dest);
 }
