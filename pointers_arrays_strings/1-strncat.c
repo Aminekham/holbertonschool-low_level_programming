b2b0e31ed9535191d819e73d4cd24e203f1e92e7
Aminekham <lkham0508@gmail.com>
2022-10-12 12:02:07 +0200

    this is my n concat

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,19 +1,19 @@
 #include "main.h"
 #include <string.h>
 /**
- * _strncat - concat to n 
- * 
+ * _strncat - concat to n
+ *
  * @dest: - string to concat in 
  * 
  * @src: - string to concat
- * 
+ *
  * @n: - integer to determinate how much we concat
- * 
+ *
  *  Return: the final string
  */
 char *_strncat(char *dest, char *src, int n)
 {
-    int i, mn;
+	int i, mn;
 
 	mn = strlen(dest);
 	for (i = 0; i < n; i++)
