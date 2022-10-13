f1698266d5375e2e6913f2b681d67dd1f0243fbd
Aminekham <lkham0508@gmail.com>
2022-10-13 14:32:33 +0200

    get it uppercase

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,23 +1,23 @@
 #include "main.h"
 #include <string.h>
 /**
- * string_toupper : get it all to uppercase
- * 
+ *string_toupper : get it all to uppercase
+ *
  * @str: - the string to upper
- * 
- *  Return : the string fully upper
+ *
+ * Return : the string fully upper
  */
 char *string_toupper(char *str)
 {
-    int l, i;
+	int l, i;
 
-    l = strlen(str);
-    for (i = 0; i < l; i++)
-    {
-        if (str[i] > 96 && str[i] < 123 )
-        {
-            str[i] = str[i] - 32;
-        }
-    }
-    return (str);
+	l = strlen(str);
+	for (i = 0; i < l; i++)
+	{
+		if (str[i] > 96 && str[i] < 123)
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
 }
