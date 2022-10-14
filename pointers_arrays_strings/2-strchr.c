8afc97ce0f7433a262bab75aff532110e33d60b5
Aminekham <lkham0508@gmail.com>
2022-10-14 18:33:22 +0200

    find the char

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strchr - find a char in a string
+ * 
+ * @s: - a string to search in
+ * 
+ * @c: - the char that we re searching for
+ *
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
