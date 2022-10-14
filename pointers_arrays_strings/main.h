233e2972d22faa8d2dac476cc89e8646faa54925
Aminekham <lkham0508@gmail.com>
2022-10-14 13:07:45 +0200

    the headers

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -18,3 +18,4 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
