05db1d1325e56f1a0337bd4bd2f8992b0baa86e2
Aminekham <lkham0508@gmail.com>
2022-10-14 23:55:00 +0200

    lets count

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
  * _strcmp - function to count the difference between the differences
- * 
+ *
  * @s1: - first string
- * 
+ *
  * @s2: - second string
- * 
+ *
  *  Return : difference between the last two characters
  */
 int _strcmp(char *s1, char *s2)
 {
-    while (*s1 == *s2 && *s1 !='\0' && *s2 !='\0')
-    {
-        s1++;
-        s2++;
-    }
-    return (*s1 - *s2);
+	while (*s1 == *s2 && *s1 != '\0' && *s2 != '\0')
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
