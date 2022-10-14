88e6c07cdbb99c72e28221b04b68e89cda8be819
Aminekham <lkham0508@gmail.com>
2022-10-14 13:11:31 +0200

    mem copy

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,16 +1,24 @@
 #include "main.h"
 /**
+ * _memcpy - function to copy from memory to memory
  * 
+ * @src: - the memory source
  * 
+ * @dest: - the memory destination
+ * 
+ * @n: - the number of copying
+ * 
+ * Return: return the destination variable
+ *
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    unsigned int i;
-    i = 0;
-    while (i < n)
-    {
-        *(dest + i) = *(src + i);
-        i++;
-    }
-    return (dest);
+	unsigned int i;
+	i = 0;
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
 }
