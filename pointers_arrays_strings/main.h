29f324edd4adc7a455e26003d77ed7c719f51ad7
Aminekham <lkham0508@gmail.com>
2022-10-14 12:46:14 +0200

    set the memory

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -16,3 +16,5 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
+char *cap_string(char *);
+char *_memset(char *s, char b, unsigned int n);
