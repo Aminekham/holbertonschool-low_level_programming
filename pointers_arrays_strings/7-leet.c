7ecf6aa9002b1587992adc02d7fdceddf408f072
Aminekham <lkham0508@gmail.com>
2022-10-15 18:33:04 +0200

    encode to leet

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,26 +1,30 @@
 #include "main.h"
 #include <string.h>
 /**
+ * leet - function to encode to leet 
  * 
+ * @str: - string to encode
  * 
+ * Return: return the string encoded
+ *
  */
 char *leet(char *str)
 {
-	int l ,i, l2, j;
+	int l, i, l2, j;
 	char *maj, *min, *leet;
 
 	maj = "AEOTL";
 	min = "aeotl";
 	leet = "43071";
 	l = strlen(str);
-	l2 = strlen (maj);
+	l2 = strlen(maj);
 	for (i = 0; i < l; i++)
 	{
 		for (j = 0; j < l2; j++)
 		{
-			if (str [i] == maj [j] || str[i] == min [j])
+			if (str[i] == maj[j] || str[i] == min[j])
 			{
-				str[i] = leet [j];
+				str[i] = leet[j];
 			}
 		}
 	}
