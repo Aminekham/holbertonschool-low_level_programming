cc3d5f1ada00c85681873ff389777737b408e086
Aminekham <lkham0508@gmail.com>
2022-10-17 11:14:43 +0200

    the puts with recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * 
+ * 
+ */
+void _puts_recursion(char *s)
+{
+    if (*s != '\0')
+    {
+        _putchar(*s);
+        s++;
+        _puts_recursion(s);
+    }
+    else
+    {
+        _putchar('\n');
+    }
+}
