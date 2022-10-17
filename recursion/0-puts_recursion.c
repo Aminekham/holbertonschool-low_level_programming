e59450d1513ebdb1cfcd12843017d468bf11170d
Aminekham <lkham0508@gmail.com>
2022-10-17 11:19:47 +0200

    puts with recursion function

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
- * 
- * 
+ *_puts_recursion - funtion to do the work of puts with recursion
+ *
+ * @s: - the string to print
  */
 void _puts_recursion(char *s)
 {
-    if (*s != '\0')
-    {
-        _putchar(*s);
-        s++;
-        _puts_recursion(s);
-    }
-    else
-    {
-        _putchar('\n');
-    }
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
