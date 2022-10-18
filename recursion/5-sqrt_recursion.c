2400e85bfe7ea64fe3dea86b6e098cbde3a6acd5
Aminekham <lkham0508@gmail.com>
2022-10-18 10:46:57 +0200

    give the square root with recursion

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,10 +1,12 @@
 #include "main.h"
 /**
- *_sqrt_recursion - function to give the square root of n
+ *sqrts - function to help comparing two integers to find the square root
  *
- * @n: - the integer to figure the square root of
+ * @r: - the integer to move further and give us the return needed 
+ * 
+ * @m: - the integer to find the square root of
  *
- * Return: the square root of n
+ * Return: the function itself to test again
  */
 int sqrts(int r, int m)
 {
@@ -22,6 +24,13 @@ int sqrts(int r, int m)
 		return (sqrts(r, m));
 	}
 }
+/**
+ *_sqrt_recursion - function to give the square root of n
+ *
+ * @n: - the integer to figure the square root of
+ *
+ * Return: the square root of n
+ */
 int _sqrt_recursion(int n)
 {
 	return (sqrts(1, n));
