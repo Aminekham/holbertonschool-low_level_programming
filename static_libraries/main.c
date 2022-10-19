4ef4ac4b8ce5fd69fce68bb5ecc3d812668bb8a8
Aminekham <lkham0508@gmail.com>
2022-10-19 20:59:25 +0200

    my library

--- static_libraries/main.c
+++ static_libraries/main.c
@@ -1,10 +1,5 @@
 #include "main.h"
 
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
 int main(void)
 {
     _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
