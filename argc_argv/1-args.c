6192fc4dc8595b6fdf2250269d16d3587e731c4b
Aminekham <lkham0508@gmail.com>
2022-10-20 12:19:24 +0200

    lets count the arguments

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 /**
- * 
- * 
+ *main - count the arguments
+ *
+ * @argc: - counting the number of elements in argv
+ *
+ * @argv: - the vector of commands
+ *
  */
 int main(int argc, char *argv[])
 {
-    int i;
-    for (i = 1;i < argc;i++)
-    {
-        argv++;
-    }
-    printf("%d\n", i - 1);
-    return (0);
+	int i;
+	for (i = 1; i < argc; i++)
+	{
+		argv++;
+	}
+	printf("%d\n", i - 1);
+	return (0);
 }
