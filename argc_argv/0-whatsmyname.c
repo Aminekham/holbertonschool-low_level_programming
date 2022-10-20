ca9040d4faf7f2fdc966bfc7737d824f4816b86b
Aminekham <lkham0508@gmail.com>
2022-10-20 12:00:00 +0200

    program name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 /**
  *main - the main function to find the name
- * 
+ *
  * @argc: - the argc to count
- * 
+ *
  * @argv:- the argv is the arguments vector
- * 
+ *
+ * Return: the zero for int main
  */
 int main(int argc, char *argv[])
 {
 	argc = 0;
 	printf("%s\n", argv[argc]);
-	return(0);
+	return (0);
 }
