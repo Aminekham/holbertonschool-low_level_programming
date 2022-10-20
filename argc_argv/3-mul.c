347221907d7f20fb962c439ca613c1a61acd2399
Aminekham <lkham0508@gmail.com>
2022-10-20 15:50:23 +0200

    multip

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * 
- * 
+ *main - the main function to find the name
+ *
+ * @argc: - the argc to count
+ *
+ * @argv:- the argv is the arguments vector
+ *
+ * Return: the zero for int main
  */
 int main(int argc, char *argv[])
 {
-    int r;
-    argc = 1;
-    r = atoi(argv[argc]) *atoi(argv[argc + 1]);
-    printf("%d\n", r);
-    return (0);
+	int r;
+	if (argc < 2)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		argc = 1;
+		r = atoi(argv[argc]) * atoi(argv[argc + 1]);
+		printf("%d\n", r);
+	}
+	return (0);
 }
