87a925e376eb15b8fab2df4d3f511e7b52f12efa
Aminekham <lkham0508@gmail.com>
2022-10-23 16:18:56 +0200

    function to copy the string to the new allocated memory

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *_strdup - function to copy the contents of the input string to an allocated memory
+ *
+ * @str: - the string to copy
+ *
+ * Return : returns a pointer to the copied string
+ */
+char *_strdup(char *str)
+{
+	int i, l;
+	char *newstr;
+	i = 0;
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		l = strlen(str);
+		newstr = malloc(sizeof(char) * l);
+		while (i < l)
+		{
+			*(newstr + i) = *(str + i);
+			i++;
+		}
+	}
+	return (newstr);
+	free(newstr);
+}
