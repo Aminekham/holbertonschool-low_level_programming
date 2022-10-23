ccb339cc6693c6fdd62199190edc11f5327f9cb9
Aminekham <lkham0508@gmail.com>
2022-10-23 15:40:20 +0200

    create array with allocated memory at the heap

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,7 +1,12 @@
 #include "main.h"
 /**
+ *create_array : allocate memory in the heap for an array
  * 
+ * @size: gives the size of that array
  * 
+ * @c: the character to fill the array with
+ * 
+ * Return : returns a pointer to the character in that array
  * 
  */
 char *create_array(unsigned int size, char c)
