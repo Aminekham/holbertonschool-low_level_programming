31a5e169854cf40b8024838ab089d5b2dc93fd12
Aminekham <lkham0508@gmail.com>
2022-10-24 14:35:56 +0200

    do that grid

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ *alloc_grid - allocating a 2D table
+ *
+ * @width: - the width of the table
+ *
+ * @height: - the height of the table
+ *
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **grid;
+
+	if (height <= 0 || width <= 0)
+	{
+		return (NULL);
+	}
+	grid = malloc(sizeof(int) * height);
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+	}
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+	free(grid);
+}
