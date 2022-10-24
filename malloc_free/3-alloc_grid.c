9d02c3f32c2e8e16068857e4cc6733c6ba1460c3
Aminekham <lkham0508@gmail.com>
2022-10-24 14:53:27 +0200

    do that grid

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	grid = malloc(sizeof(int *) * height);
+	grid = malloc(sizeof(int*) * height);
 	if (grid == NULL)
 	{
 		return (NULL);
