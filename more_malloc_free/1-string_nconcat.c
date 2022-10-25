d517d39a693e8d346d37ceee12f5c8ddab95208d
Aminekham <lkham0508@gmail.com>
2022-10-25 16:43:12 +0200

    string concat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -16,7 +16,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		n = l1;
 	}
-	sum = malloc(l + n + 1);
+	sum = malloc((l + n + 1) * sizeof(char));
 	if (sum == NULL)
 	{
 		return(NULL);
