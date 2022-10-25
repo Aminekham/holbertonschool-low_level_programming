405c0c5048730d83531ae0934222dccc8645aaeb
Aminekham <lkham0508@gmail.com>
2022-10-25 16:38:24 +0200

    string concat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -11,12 +11,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	l = strlen(s1);
 	l1 = strlen(s2);
-	j = 0;
+	
 	if (n > l1)
 	{
 		n = l1;
 	}
-	sum = malloc(l + n + 2);
+	sum = malloc(l + n + 1);
 	if (sum == NULL)
 	{
 		return(NULL);
@@ -31,11 +31,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	else
 	{
-		for (i = 0; i <= l; i++)
+		j = 0;
+		for (i = 0; i < l; i++)
 		{
 			sum[i] = s1[i];
 		}
-		for (i = i ;i < l + n + 1; i++)
+		for (i = i;i < (l + n); i++)
 		{
 			sum[i] = s2[j];
 			j++;
