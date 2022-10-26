d9bdf505e80de9bb660583d84b7e248cfd45e6e7
Aminekham <lkham0508@gmail.com>
2022-10-26 18:43:32 +0200

    from min to max

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -13,9 +13,13 @@ int *array_range(int min, int max)
 	int *res;
 	int diff, i, j;
 
+	if (min > max)
+	return (NULL);
 	j = 0;
 	diff = max - min;
 	res = malloc((diff + 1) * sizeof(int));
+	if (res == NULL)
+	return (NULL);
 	for (i = min; i <= max; i++)
 	{
 		res[j] = i;
