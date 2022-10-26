4d7638bbc103a0c18d6c734119d59cec714f4215
Aminekham <lkham0508@gmail.com>
2022-10-26 11:23:01 +0200

    char malloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
-	return(NULL);
+		return (NULL);
 	sm = (char *)malloc(nmemb * size);
 	if (!sm)
 		return (NULL);
