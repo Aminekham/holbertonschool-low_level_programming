b47fc571a4228b0c6e20129126c037688b2f0900
Aminekham <lkham0508@gmail.com>
2022-10-28 12:16:13 +0200

    display the name of the file

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #define file __FILE__
+/**
+ *
+ *main - the main function
+ *
+ * Return: - returns 0
+ */
 int main()
 {
-    printf("%s\n", file);
-    return (0);
+	printf("%s\n", file);
+	return (0);
 }
