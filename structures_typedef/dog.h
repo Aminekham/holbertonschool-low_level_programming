53a11b307a3a1af89908c68594faab0f4cd7f900
Aminekham <lkham0508@gmail.com>
2022-11-01 13:27:24 +0100

    header file

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,6 +2,11 @@
 #define DOG
 /**
  *struct dog - a structure defining the features of a dog
+ * @name: -name of the dog
+ * 
+ * @age: -the age of our dog
+ * 
+ * @owner: -the name of its owner
  */
 struct dog
 {
@@ -9,12 +14,5 @@ struct dog
 	float age;
 	char *owner;
 };
-/**
- * @name: -name of the dog
- * 
- * @age: -the age of our dog
- * 
- * @owner: -the name of its owner
- */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
