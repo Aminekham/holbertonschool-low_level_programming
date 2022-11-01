3c805d0fc8caf302892b6d3770a4bfb1a38f82bf
Aminekham <lkham0508@gmail.com>
2022-11-01 13:14:59 +0100

    init for our dog

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "dog.h"
 /**
- *init_dog - function to give the infos of a dog 
+ *init_dog - function to give the infos of a dog
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
