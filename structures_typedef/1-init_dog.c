eda0382406d2c61c3dfe19257f7342b602996cb9
Aminekham <lkham0508@gmail.com>
2022-11-01 13:28:45 +0100

    init to our dog

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -3,12 +3,12 @@
 /**
  *init_dog - function to give the infos of a dog
  *
- * @d: -the name of the var 
- * 
+ * @d: -the name of the var
+ *
  * @name: -name of the dog
- * 
+ *
  * @age: -the age of our dog
- * 
+ *
  * @owner: -the name of its owner
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
