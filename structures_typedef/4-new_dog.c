d7b74ada39341462e1003611460c3b8905c2e317
Aminekham <lkham0508@gmail.com>
2022-11-02 12:53:29 +0100

    dog situation

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,19 +12,42 @@ dog_t *new_dog(char *name, float age, char *owner)
 	int i, j;
 
 	new->age = age;
+	if (new == NULL)
+	{
+		return(NULL);
+	}
 	new->name = malloc(sizeof(*name));
+	if (new->name == NULL)
+	{
+		return(NULL);
+	}
 	new->owner = malloc(sizeof(*owner));
+	if (new->owner == NULL)
+	{
+		return(NULL);
+	}
 	i = 0;
 	while (*(name + i) != '\0')
 	{
 		*(new->name + i) = *(name + i);
 		i++;
 	}
+	if (new->name == NULL && name == NULL)
+	{
+		free(new);
+		return(NULL);
+	}
 	j = 0;
 	while (*(owner + j) != '\0')
 	{
 		*(new->owner + j) = *(owner + j);
 		j++;
+	}
+	if (new->owner == NULL && owner == NULL)
+	{
+		free(new);
+		free(name);
+		return(NULL);
 	}
 	return(new);
 }
