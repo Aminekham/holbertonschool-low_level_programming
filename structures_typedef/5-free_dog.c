112893c46cd6f2be63be66ca8dc9470491171bce
Aminekham <lkham0508@gmail.com>
2022-11-03 23:49:06 +0100

    lets free all the dogs

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <dog.h>
+/**
+ *free_dog - frees all the allocated memory
+ *
+ *@d: - the saved dogs
+ *
+ */
+void free_dog(dog_t *d)
+{
+	while (d != NULL)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
