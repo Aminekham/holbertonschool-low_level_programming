0df33eae70a8886b0cfb8368dedb0faed453f366
Aminekham <lkham0508@gmail.com>
2022-11-03 13:46:30 +0100

    print the numbers gived as arguments

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -2,24 +2,26 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 /**
- * 
- * 
- * 
- * 
+ *print_numbers - printing the numbers donated as arguments
+ *
+ * @n: - the number of arguments
+ *
+ * @separator: - the seperator we should use
+ *
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    va_list p;
-    unsigned int i;
+	va_list p;
+	unsigned int i;
 
-    va_start(p, n);
-    for (i = 0; i < n; i++)
-    {
-        if (i == n - 1)
-        {
-            printf("%d\n",va_arg(p, int));
-        }
-        printf("%d%c ",va_arg(p, int),*separator);
-    }
-    va_end(p);
+	va_start(p, n);
+	for (i = 0; i < n; i++)
+	{
+		if (i == n - 1)
+		{
+			printf("%d\n", va_arg(p, int));
+		}
+		printf("%d%c ", va_arg(p, int), *separator);
+	}
+	va_end(p);
 }
