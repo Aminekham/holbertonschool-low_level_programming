806fc4a7320faebd2f60e64d3b4c04dd77759237
Aminekham <lkham0508@gmail.com>
2022-11-04 09:57:38 +0100

    printing someones's name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include "function_pointers.h"
 /**
- * 
- * 
- * 
- * 
+ *print_name - printing the name of someone
+ *
+ * @name: - the name of the person
+ *
+ * @f: - the function pointer that we will use
+ *
  */
 void print_name(char *name, void (*f)(char *))
 {
-    f(name);
+	f(name);
 }
