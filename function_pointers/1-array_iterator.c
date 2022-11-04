f891dcc359442be9ad71a11ba8225d956244b91a
Aminekham <lkham0508@gmail.com>
2022-11-04 10:12:49 +0100

    pointer of function used on an array

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -13,6 +13,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
 	for (i = 0; i < size; i++)
 	{
 		action(*(array + i));
