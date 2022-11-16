1bca4e9190dfdc7671460f8f6211cac25c02ff86
Aminekham <lkham0508@gmail.com>
2022-11-16 19:13:18 +0100

    adding the new node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,7 +12,7 @@ list_t *add_node(list_t **head, const char *str)
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
 	new->str = strdup(str);
 	new->len = strlen(str);
