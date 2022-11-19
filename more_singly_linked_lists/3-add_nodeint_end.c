5a942db78562481b820fe6b695ec6abc8fb813ae
Aminekham <lkham0508@gmail.com>
2022-11-19 16:00:19 +0100

    fixing the betty coding style

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -12,7 +12,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	newend = malloc(sizeof(listint_t));
 	if (newend == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
 	newend->n = n;
 	newend->next = NULL;
