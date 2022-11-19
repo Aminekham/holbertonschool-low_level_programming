84e7655cc787ce01e8b895b281e240f9f96ccba5
Aminekham <lkham0508@gmail.com>
2022-11-19 15:40:16 +0100

    fixing betty coding style

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -4,18 +4,18 @@
  *@head: the head of the list
  *@n: the integer used as value
  *Return: the adress of the new added node
-*/
+ */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-    listint_t *newint;
+	listint_t *newint;
 
-    newint = malloc(sizeof(listint_t));
-    if (newint == NULL)
-    {
-        return(NULL);
-    }
-    newint->n = n;
-    newint->next = *head;
-    *head = newint;
-    return(*head);
+	newint = malloc(sizeof(listint_t));
+	if (newint == NULL)
+	{
+		return (NULL);
+	}
+	newint->n = n;
+	newint->next = *head;
+	*head = newint;
+	return (*head);
 }
