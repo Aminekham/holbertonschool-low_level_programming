38c34fa233105f720c7d7dae1d68c23c5523c31d
Aminekham <lkham0508@gmail.com>
2022-11-19 15:37:35 +0100

    add a node at the start of a singly linked list

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 /**
- * 
- * 
- * 
- * 
+ *add_nodeint - adding a node at the begining of the list
+ *@head: the head of the list
+ *@n: the integer used as value
+ *Return: the adress of the new added node
 */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
