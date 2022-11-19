b991f85ab15247be9cb277053b86c203adbd6cc4
Aminekham <lkham0508@gmail.com>
2022-11-19 14:58:37 +0100

    fixing the kernel style

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -2,10 +2,12 @@
 /**
  *listint_len - calculates the number of elements in a singly linked list
  *@h: - a pointer on the head of the singly linked list
+ *Return: returns the number of elemnts
  */
 size_t listint_len(const listint_t *h)
 {
 	int c = 0;
+
 	while (h)
 	{
 		h = h->next;
