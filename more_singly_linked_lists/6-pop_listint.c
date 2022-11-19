724fbd4d2526f49f9baf153fd5afe0b5aa0084a1
Aminekham <lkham0508@gmail.com>
2022-11-19 22:35:11 +0100

    deleting the first node

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ *pop_listint - deleting the first node
+ *@head: the head of the linked list
+ *Return: the data stored in the node
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t **hd = NULL;
+	int v;
+
+	hd = (*head)->next;
+	v = (*head)->n;
+	free(*head);
+	*head = hd;
+	return (v);
+}
