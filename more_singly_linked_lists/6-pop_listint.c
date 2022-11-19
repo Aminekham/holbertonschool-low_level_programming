7a785349a4b82b54c9e8551828b9bfb2b8c61fa4
Aminekham <lkham0508@gmail.com>
2022-11-19 22:44:46 +0100

    popping the first node

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -6,12 +6,12 @@
  */
 int pop_listint(listint_t **head)
 {
-	listint_t **hd = NULL;
+	listint_t *hd = NULL;
 	int v;
 
-	*hd = (*head)->next;
+	hd = (*head)->next;
 	v = (*head)->n;
 	free(*head);
-	*head = *hd;
+	*head = hd;
 	return (v);
 }
