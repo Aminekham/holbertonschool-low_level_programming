4f29908ead78a93c5d8b0f136d7ee3705ecf1823
Aminekham <lkham0508@gmail.com>
2022-11-23 13:10:27 +0100

    file creation with text content

---
+++ file_io/1-create_file.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *create_file - file creation with a text content
+ *@filename: the name of the new file
+ *@text_content: the content of the new file
+ *Return: the number of written characters
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int f, x, l;
+
+	f = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0666);
+	if (f == NULL)
+	{
+		return (-1);
+	}
+	l = strlen(text_content);
+	x = write(f, text_content, l);
+	if (x = -1)
+	{
+		return (-1);
+	}
+	return (1);
+}
