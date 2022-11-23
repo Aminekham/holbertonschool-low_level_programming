d7f3e78a911da4fb91758d3c5dd8685d37b096ae
Aminekham <lkham0508@gmail.com>
2022-11-23 14:10:11 +0100

    appending a text errors fixing

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -4,26 +4,26 @@
  *@filename: - the file name
  *@text_content: - the text we want to append
  *Return: the value true or false
-*/
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int f, l;
+	int f, l;
 
-    if (filename == NULL)
-    {
-        return(-1);
-    }    
-    f = open(filename, O_APPEND | O_WRONLY);
-    if (f == -1)
-    {
-        return(-1);
-    }
-    if (text_content == NULL)
-    {
-        return(1);
-    }
-    l = strlen(text_content);
-    write(f, text_content, l);
-    close(f);
-    return(1);
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	f = open(filename, O_APPEND | O_WRONLY);
+	if (f == -1)
+	{
+		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		return (1);
+	}
+	l = strlen(text_content);
+	write(f, text_content, l);
+	close(f);
+	return (1);
 }
