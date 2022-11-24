0cd69ce0c81e08c655f79913b3c6e17caa198cfa
Aminekham <lkham0508@gmail.com>
2022-11-24 17:23:19 +0100

    fixing the betty style

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -2,17 +2,17 @@
 /**
  *print_dlistint - printing the whole doubly linked list
  *@h: the head of the doubly linked list
- *Returns: the number of elements of the doubly linked list
-*/
+ *Return: the number of elements of the doubly linked list
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
-    int i = 0;
+	int i = 0;
 
-    while(h)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        i++;
-    }
-    return(i);
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
