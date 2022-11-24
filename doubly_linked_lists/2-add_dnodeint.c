ebe9d6be43bceb89138fc09869185fc880678cb0
Aminekham <lkham0508@gmail.com>
2022-11-24 17:42:24 +0100

    adding the new node

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ *add_dnodeint - adding a node at the start of the doubly linked list
+ *@head: the head of the doubly linked list
+ *@n: the value of the new added node
+ *Return: returns the adress of the new node
+*/
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+    dlistint_t *new;
+
+    new->n = n;
+    new->next = *head;
+    new->prev = NULL;
+    *head = new;
+    return(*head);
+}
