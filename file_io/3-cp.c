0623cd0ae277d15a4516112450befb00fbbfbeba
Aminekham <lkham0508@gmail.com>
2022-11-24 16:54:21 +0100

    the copy file function

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -7,7 +7,7 @@
 */
 void the_cp(int argc, char **argv)
 {
-    int f, f1, r, r1, i = 0;
+    int f, f1, i = 0;
     char *bf;
 
     if (argc > 3)
@@ -16,28 +16,28 @@ void the_cp(int argc, char **argv)
         exit(97);
     }
     f = open(argv[1], O_RDONLY);
-    if (f = -1)
+    if (f == -1)
     {
         printf("Error: Can't read from file NAME_OF_THE_FILE\n");
         exit(98);
     }
     f1 = open(argv[2], O_WRONLY | O_TRUNC);
-    if (f1 = -1)
+    if (f1 == -1)
     {
         f1 = open(argv[2], O_CREAT | O_WRONLY, 0664);
-        if (f1 = -1)
+        if (f1 == -1)
         {
             printf("Error: Can't write to NAME_OF_THE_FILE\n");
             exit(98);
         }
     }
     bf = malloc(1024);
-    r = read(f, bf, 1024);
+    read(f, bf, 1024);
     while (*bf != EOF)
     {
         i = i + 1024;
         bf = realloc(bf, i);
-        r = read(f, bf, i);
+        read(f, bf, i);
     }
     dprintf(f1, "%s", bf);
 }
