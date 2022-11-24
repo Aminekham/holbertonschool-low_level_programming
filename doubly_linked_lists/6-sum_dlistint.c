f8b3938bdcd398cf835c9cab9bc04485bfd02600
Aminekham <lkham0508@gmail.com>
2022-11-24 20:44:05 +0100

    returns the sum of the given doubly linked list

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ *sum_dlistint - returns the sum of the data in the doubly linked list
+ *@head: the head of the doubly linked list
+ *Return: returns the value of the sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *save;
+	int s = 0;
+
+	save = head;
+	while (save->next)
+	{
+		s = s + save->n;
+		save = save->next;
+	}
+	s = s + save->n;
+	return (s);
+}
