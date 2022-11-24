5680519f89f9cadf1ad8037ff7123b9c13ce03a9
Aminekham <lkham0508@gmail.com>
2022-11-24 17:18:39 +0100

    printing a doubly linked list

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * 
+ * 
+ * 
+ * 
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+    int i = 0;
+
+    while(h)
+    {
+        printf("%d\n", h->n);
+        h = h->next;
+        i++;
+    }
+    return(i);
+}
