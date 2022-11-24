b23b52dd456bc15e7f8d6b4d6ce534b228cb314b
Aminekham <lkham0508@gmail.com>
2022-11-24 21:13:25 +0100

    insert in the list

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,31 +1,32 @@
 #include "lists.h"
 /**
- * 
- * 
- * 
- * 
-*/
+ *insert_dnodeint_at_index - a function that inserts a new node
+ *@h: the head of the linked list
+ *@idx: the index we want to insert in
+ *@n: the value of the new node
+ *Return: the adress of the new node
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t **save, *new;
-    unsigned int i = 0;
+	dlistint_t **save, *new;
+	unsigned int i = 0;
 
-    new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-    {
-        return(NULL);
-    }
-    new->n = n;
-    save = h;
-    while (i < idx - 1)
-    {
-        save = &(*save)->next;
-        i++;
-    }
-    new->next = (*save)->next;
-    new->prev = *save;
-    (*save)->next = new;
-    save = &(*save)->next;
-    (*save)->prev = new;
-    return(new);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	save = h;
+	while (i < idx - 1)
+	{
+		save = &(*save)->next;
+		i++;
+	}
+	new->next = (*save)->next;
+	new->prev = *save;
+	(*save)->next = new;
+	save = &(*save)->next;
+	(*save)->prev = new;
+	return (new);
 }
