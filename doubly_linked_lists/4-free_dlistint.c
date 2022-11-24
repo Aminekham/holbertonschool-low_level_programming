e9039f47ebd3459ca0696cdd2e42fdd7fbe7d6e9
Aminekham <lkham0508@gmail.com>
2022-11-24 19:12:45 +0100

    a function to free the doubly linked list

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,18 +1,18 @@
 #include "lists.h"
 /**
  *free_dlistint - free a whole doubly linked list
- *@head: the head of the linked list 
-*/
+ *@head: the head of the linked list
+ */
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *s;
+	dlistint_t *s;
 
-    s = malloc(sizeof(dlistint_t));
-    while(head)
-    {
-        s->next = head->next;
-        free(head);
-        head = s->next;
-    }
-    free(s);
+	s = malloc(sizeof(dlistint_t));
+	while (head)
+	{
+		s->next = head->next;
+		free(head);
+		head = s->next;
+	}
+	free(s);
 }
