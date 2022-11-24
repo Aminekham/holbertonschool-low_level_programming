7c46e5ce4a2c48c74876eb6228a49f09c7a15fe0
Aminekham <lkham0508@gmail.com>
2022-11-24 18:16:56 +0100

    fixing

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
     }
     new->n = n;
     new->next = *head;
-    (*head)->prev = new;
+    (*head)->prev = *new;
     new->prev = NULL;
     *head = new;
     return(*head);
