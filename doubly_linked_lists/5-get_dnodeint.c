702534d3041c3394637c78cba16152bea265a4ca
Aminekham <lkham0508@gmail.com>
2022-11-24 20:17:08 +0100

    return the desired index

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,19 +1,19 @@
 #include "lists.h"
 /**
- * 
- * 
- * 
- * 
-*/
+ *get_dnodeint_at_index - returns the desired node
+ *@head: the head of the doubly linked list
+ *@index: the desired index
+ *Return: the node with the index : index
+ */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    unsigned int i;
-    dlistint_t *save;
+	unsigned int i;
+	dlistint_t *save;
 
-    save = head;
-    for (i = 0; i <= index; i++)
-    {
-        save = save->next;
-    }
-    return(save);
+	save = head;
+	for (i = 0; i <= index; i++)
+	{
+		save = save->next;
+	}
+	return (save);
 }
