b0e47f397ba3ed6bd70db450790f950fcf62ccc6
Aminekham <lkham0508@gmail.com>
2022-11-25 18:03:02 +0100

    try this

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -27,6 +27,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		save = &(*save)->next;
 		i++;
 	}
+	if ((*save)->next = NULL)
+	{
+		return(add_dnodeint_end(h, n));
+	}
 	new->next = (*save);
 	new->prev = ((*save)->prev);
 	((*save)->prev)->next = new; 
