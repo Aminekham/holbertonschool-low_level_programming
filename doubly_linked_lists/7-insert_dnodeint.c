16cc9e259f2ad56956bbdf384f5cb886d40b3c99
Aminekham <lkham0508@gmail.com>
2022-11-25 18:18:57 +0100

    try this

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -10,7 +10,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t **save, *new;
 	unsigned int i = 0;
-
+	if(!h||!*h)
+	return(NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 	{
