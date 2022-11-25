b2a9c1aa86e92f87c55b9fd056fd8c9b3cf8424b
Aminekham <lkham0508@gmail.com>
2022-11-25 18:19:56 +0100

    try this

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -10,8 +10,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t **save, *new;
 	unsigned int i = 0;
-	if(!h||!*h)
-	return(NULL);
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 	{
@@ -19,24 +18,24 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	if (idx == 0)
 	{
-		return (add_dnodeint(h, n));
+		return(add_dnodeint(h, n));
 	}
 	new->n = n;
 	save = h;
 	while (i < idx)
 	{
-		if (!save)
-			return (NULL);
+		if(!save)
+		return(NULL);
 		save = &(*save)->next;
 		i++;
 	}
 	if ((*save)->next == NULL)
 	{
-		return (add_dnodeint_end(h, n));
+		return(add_dnodeint_end(h, n));
 	}
 	new->next = (*save);
 	new->prev = ((*save)->prev);
-	((*save)->prev)->next = new;
+	((*save)->prev)->next = new; 
 	(*save)->prev = new;
 	return (new);
 }
