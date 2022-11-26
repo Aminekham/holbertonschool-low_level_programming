06e24ecf3b461f75c323ca5dddda98b9e535723d
Aminekham <lkham0508@gmail.com>
2022-11-26 22:32:05 +0100

    creating a hash table

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,10 +11,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 	size_t i;
 
 	new = malloc(sizeof(hash_table_t));
-	new->array = malloc(sizeof(hash_node_t *) * size);
-    if (new == NULL || new->array == NULL)
+    if (new == NULL)
     {
         return(NULL);
+    }
+	new->array = malloc(sizeof(hash_node_t *) * size);
+    if (new->array == NULL)
+    {
+        return (NULL);
     }
 	for (i = 0; i < size; i++)
 	{
