7d91959ab0946bb6620740fc38d0523b9ab5cd71
Aminekham <lkham0508@gmail.com>
2022-11-26 22:33:58 +0100

    fixing betty style

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,25 +1,24 @@
 #include "hash_tables.h"
 /**
- * 
- * 
- * 
- * 
-*/
+ *hash_table_create - creating a hash table
+ *@size: the size of the hash table
+ *Return: the hash table created
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new = NULL;
 	size_t i;
 
 	new = malloc(sizeof(hash_table_t));
-    if (new == NULL)
-    {
-        return(NULL);
-    }
+	if (new == NULL)
+	{
+		return (NULL);
+	}
 	new->array = malloc(sizeof(hash_node_t *) * size);
-    if (new->array == NULL)
-    {
-        return (NULL);
-    }
+	if (new->array == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
 		new->array[i] = NULL;
