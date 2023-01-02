fce8b00e42aa851e2e228d048aae3f2a023ea9ad
Aminekham <lkham0508@gmail.com>
2023-01-02 19:59:29 +0100

    fixing the coding style

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -4,6 +4,7 @@
  *@array: the array to search in
  *@size: the size of that array
  *@value: the value that we are searching for
+ *Return: returns an int that represents the index of the searched value
  */
 int linear_search(int *array, size_t size, int value)
 {
