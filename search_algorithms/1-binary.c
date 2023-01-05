6817a192181cad025c7a5e6bdac4bc533bc4da62
Aminekham <lkham0508@gmail.com>
2023-01-05 13:34:49 +0100

    the binary search

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -17,7 +17,7 @@ int binary_search(int *array, size_t size, int value)
 	}
 	while (L <= R)
 	{
-		printf("Searching in array :");
+		printf("Searching in array: ");
 		for (i = L; i <= R; i++)
 		{
 			if (i == R)
